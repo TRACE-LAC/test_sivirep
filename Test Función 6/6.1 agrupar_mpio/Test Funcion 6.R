8f594b41994cb2d6313609d59e3ccf1f134160ad
juanitaromerog <juanita.romerog@javeriana.edu.co>
2024-03-05 13:49:00 -0500

    Test Función 6
    Test de la función dist_esp_dept  con su respectivo CVS y Excel y Test de la función plot_map con su respectivo CVS y Excel

---
+++ Test Función 6/6.1 agrupar_mpio/Test Funcion 6.R
@@ -0,0 +1,171 @@
+library(sivirep)
+
+# Inicial ------------------------------------------------------------------
+lista_eventos <- list_events()
+municipios <- read.csv("codigos_municipios.csv")                    # Tabla de todos los municipios del pais y su dpto
+municipios <- head(municipios,-5)                                   # Ignorar las últimas 5 filas porque no son útiles
+nom_departamentos <- unique(municipios$Nombre.Departamento)         # Remover duplicados para obtener todos los departamentos
+nom_departamentos <- chartr("ÁÉÍÓÚ", "AEIOU", nom_departamentos)    # Quitar tildes
+nom_departamentos[3] <- "BOGOTA"                                    # Escribir Bogota corto
+nom_departamentos[28] <- "SAN ANDRES"                               # Escribir San Andres corto
+cod_departamentos <- unique(municipios$Código.Departamento)
+
+# Funciones para buscar errores --------------------------------------------
+#Ty import
+try_importdataevent <- function(desease,year) {
+  tryCatch(
+    {
+      data_event <-  import_data_event(year = year, nombre_event = desease)
+      list(data=data_event,res="Success",msg="Success importing")                     # Devolver como resultado los datos, "Success" y un mensaje las dimensiones del data frame
+    },
+    error = function(e){
+      err_msg = paste("Error import_data_event",desease,year,": ",e)                         # Mensaje de error
+      print(err_msg)
+      list(data="",res="Error",msg=e)                                           # Devolver como resultado "Error" y el mensaje de error
+    },
+    warning = function(w){
+      wrn_msg = paste("Warning import_data_event",desease,year,": ",w)                       # Mensaje de warning
+      print(wrn_msg)
+      list(data=data_event,res="Warning",msg=w)                                 # Devolver como resultado los datos,  "warning" y el mensaje de warning
+    }
+  )
+}
+
+# Probar limpiar_data 
+try_limpiardata <- function(data_event, desease, year){
+  tryCatch(
+    {
+      data_event_limp <- limpiar_data_sivigila(data_event = data_event)
+      list(data=data_event_limp,res="Success",msg="Success cleaning")                 # Devolver como resultado los datos, "Success" y un mensaje las dimensiones del data frame
+    },
+    error = function(e){
+      err_msg = paste("Error limpiar_data_sivigila",desease,year,":",e)              # Mensaje de error
+      print(err_msg)
+      list(data="",res="Error",msg=e)                                           # Devolver como resultado "Error" y el mensaje de error
+    },
+    warning = function(w){
+      wrn_msg = paste("Warning limpiar_data_sivigila ",desease,year,": ",w)                       # Mensaje de warning
+      print(wrn_msg)
+      list(data=data_event,res="Warning",msg=w)                                 # Devolver como resultado los datos,  "warning" y el mensaje de warning
+    }
+  )
+}
+
+# Probar geo_filter 
+try_geofilter <- function(data_event_limp, dpt) {
+  tryCatch(
+    {
+      data_event_filtrada <- geo_filtro(data_event = data_event_limp, dpto = dpt)     # Intentar correr geo_filtro
+      result_dim <- dim(data_event_filtrada)                                          # Guardar las dimensiones (filas, columnas) del data frame resultante
+      succ_msg <- paste("Success geo_filtro ",result_dim[1]," rows,",result_dim[2]," columns")
+      list(data=data_event_filtrada, res="Success",msg=succ_msg)                                                # Devolver como resultado "Success" y un mensaje las dimensiones del data frame
+    },
+    error = function(e){
+      err_msg = paste("Error geo_filtro ",dpt,": ",e)                                   # Mensaje de error
+      print(err_msg)
+      list(data= "", res="Error",msg=e)                                                    # Devolver como resultado "Error" y el mensaje de error
+    },
+    warning = function(w){
+      wrn_msg = paste("Warning geo_filtro ",dpt,": ",w)                                 # Mensaje de warning
+      print(wrn_msg)
+      list(data=data_event_filtrada, res="Warning",msg=w)                                                 # Devolver como resultado "warning" y el mensaje de warning
+    }
+  )
+}
+
+
+# 1. Funcion para correr agrupar_mpio y que diga si hay error
+try_agrupar_mpio<- function (data_event_filtrada, desease, year, dpto) {
+  tryCatch(
+    {
+      dist_esp_dept <-  agrupar_mpio(data_event = data_event_filtrada,dpto=dpto)
+      result_dim <- dim(dist_esp_dept)                                          # Guardar las dimensiones (filas, columnas) del data frame resultante
+      succ_msg <- paste("Success",desease,year,dpto,":",result_dim[1]," rows,",result_dim[2]," columns")
+      print(succ_msg)
+      list(data=dist_esp_dept,res="Success",msg=succ_msg)                     # Devolver como resultado los datos, "Success" y un mensaje las dimensiones del data frame
+    },
+    error = function(e){
+      err_msg = paste("Error agrupar_mpio",desease,year,dpto, ":",e)                         # Mensaje de error
+      print(err_msg)
+      list(data="",res="Error",msg=e)                                           # Devolver como resultado "Error" y el mensaje de error
+    },
+    warning = function(w){
+      wrn_msg = paste("Warning agrupar_mpio",desease,year,dpto,": ",w)                       # Mensaje de warning
+      print(wrn_msg)
+      list(data=dist_esp_dept,res="Warning",msg=w)                                 # Devolver como resultado los datos,  "warning" y el mensaje de warning
+    }
+  )
+}
+
+excel <- read.csv("results5.csv", sep=";")                                      #Excel con los resultados de prueba.R
+no_error <- excel[excel$Resultado != "Error",]                                  #Filtrar el Excel para tener solo las variables con Success o Warning
+des_ano_no_error <-unique(no_error[,c("Desease", "Año", "Dpto")])                       #Combinaciones únicas de desease y año 
+
+
+results_dist_esp_dept <- data.frame(matrix(ncol=5, nrow=0))                   # Crear el data frame de los resultados y nombrar las columnas
+colnames(results_dist_esp_dept) <- c("Desease","Año","Departamento", "Resultado","Mensaje")
+
+#Inicio loop de todas las fila des_ano_no_error
+desease <- des_ano_no_error$Desease[1]
+year <- des_ano_no_error$Año[1]
+
+for (i in 2176:nrow(des_ano_no_error)) {                                                                            
+  newdesease <- des_ano_no_error$Desease[i]
+  newyear <- des_ano_no_error$Año[i]
+  
+  #No importar si desases y year es el mismo
+  if (newdesease!=desease | newyear!=year) { 
+    year=newyear
+    desease =newdesease
+        # Importar datos
+      data_event_result <- try_importdataevent(desease, year)
+      if (data_event_result$res == "Error"){
+        result_msg <- data_event_result$msg
+        new_row <- list(desease,year,"","Error",result_msg)    # Crear la nueva fila para los resultados con el desase. año y resultado
+        results_dist_esp_dept[nrow(results_dist_esp_dept) + 1,] <- new_row                    # Agregar la nueva fila a los resultados
+        next    
+      }
+      data_event <- data_event_result$data
+      
+      
+      # Limpiar
+      limp <- try_limpiardata(data_event = data_event, desease, year)
+      if (limp$res == "Error"){
+        result_msg <- limp$msg
+        new_row <- list(desease,year,"","Error",result_msg)    # Crear la nueva fila para los resultados con el desase. año y resultado
+        results_dist_esp_dept[nrow(results_dist_esp_dept) + 1,] <- new_row                    # Agregar la nueva fila a los resultados
+        next                                                      # Ignorar el resto del código y saltar al siguiente año
+      }
+      data_event_limp <- limp$data 
+  }
+  
+ 
+  #Probar geofiltro
+  dpto <- des_ano_no_error$Dpto[i]
+  geo <- try_geofilter(data_event = data_event_limp, dpto)
+  if (geo$res == "Error"){
+    result_msg <- geo$msg
+    new_row <- list(desease,year,dpto,"Error",result_msg)    # Crear la nueva fila para los resultados con el desase. año y resultado
+    results_dist_esp_dept[nrow(results_dist_esp_dept) + 1,] <- new_row                    # Agregar la nueva fila a los resultados
+    next                                                      # Ignorar el resto del código y saltar al siguiente año
+  }
+  data_event_filtrada <- geo$data
+  
+  #Seguir si hay error 
+  probar_agrupar_mpio<- try_agrupar_mpio(data_event_filtrada, desease, year, dpto)
+  
+  if (probar_agrupar_mpio$res == "Error") {
+    result_msg <-  probar_agrupar_mpio$msg
+    new_row <- list(desease,year,dpto, "Error",result_msg)    # Crear la nueva fila para los resultados con el desase. año y resultado
+    results_dist_esp_dept[nrow(results_dist_esp_dept) + 1,] <- new_row                    # Agregar la nueva fila a los resultados
+    next
+    
+  } else {
+    dist_esp_dept <-  probar_agrupar_mpio$data
+    new_row <- list(desease,year, dpto, probar_agrupar_mpio$res, probar_agrupar_mpio$msg)          # Crear la nueva fila para los resultados con el desase, año y resultado
+    results_dist_esp_dept[nrow(results_dist_esp_dept) + 1,] <- new_row                    # Agregar la nueva fila a los resultados
+  }
+  
+}
+#df <- apply(results_dist_esp_dept,2,as.character)
+#write.csv2(df,"Results agrupar_mpio2.CSV",row.names = FALSE, na='')
